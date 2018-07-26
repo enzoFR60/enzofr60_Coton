a93140f54aa01797a627b1a6bf2651a4e219a033
enzoFR60 <37971887+enzoFR60@users.noreply.github.com>
2018-07-26 03:35:57 +0200

    Add files via upload

---
+++ lua/entities/ati_coton_pull/init.lua
@@ -0,0 +1,31 @@
+﻿AddCSLuaFile("cl_init.lua");
+AddCSLuaFile("shared.lua");
+include("shared.lua");
+
+function ENT:Initialize()
+	self:SetModel("models/codeandmodeling_atila_pull/codeandmodeling_atila_pull.mdl");
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)	
+	self:SetUseType(SIMPLE_USE)
+	local phys = self:GetPhysicsObject()
+	phys:Wake()
+end;
+
+function ENT:Use(activator, caller)
+	local curTime = CurTime();
+		if (!self.nextUse or curTime >= self.nextUse) then
+				if not IsValid(self) then return end
+				if not IsValid(caller) then return end
+				activator:addMoney(self:GetNWInt("price"))
+				activator:SendLua("local tab = {Color(255,255,255), [[Voici ton pull]],  Color(255,255,255), [[,]], Color(255,255,255),[[ Tu à reçu "..math.Round(self:GetNWInt("price")).."€ ]], Color(128, 255, 128) } chat.AddText(unpack(tab))");
+				self:Remove();
+			self.nextUse = curTime + 1;
+		end;
+end;
+
+
+function ENT:OnTakeDamage(dmginfo)
+	self:Remove();
+end;
+
---
+++ lua/entities/ati_coton_pull/shared.lua
@@ -0,0 +1,7 @@
+ENT.Base = "base_gmodentity";
+ENT.Type = "anim";
+ENT.PrintName		= "Pull";
+ENT.Author = "Atila, enzoFR60";
+ENT.Category = "Cultivateur De Coton";
+ENT.Spawnable			= true;
+ENT.AdminSpawnable		= true;
