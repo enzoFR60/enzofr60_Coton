8deb39e3c929e4a91047c32edcc8391b1d9bc0ea
enzoFR60 <37971887+enzoFR60@users.noreply.github.com>
2018-07-26 03:34:17 +0200

    Add files via upload

---
+++ lua/entities/ati_coton_jean/shared.lua
@@ -0,0 +1,8 @@
+ENT.Base = "base_gmodentity"
+ENT.Type = "anim"
+
+ENT.PrintName		= "Jean"
+ENT.Author = "Atila, enzoFR60"
+ENT.Category = "Cultivateur De Coton"
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= true
